470be51700eca8f320d85568a10b86c8e81bc12d
Taylor Fountain <taylorfountain@Taylors-MacBook-Air-2.local>
2020-09-24 16:10:37 -0400

    Added Hyperbolic geometry

---
+++ HyperbolicGeodesic.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperbolicGeodesic : MonoBehaviour
+{
+    //Set endpoints in spherical coordinates
+    public float rho1 = 1.0f; // radius/scaling factor, <=1
+    public float ntheta1 = 0.0f; // azimuthal angle/pi
+    public float nphi1 = 0.0f; // angle of elevation/pi
+
+    public float rho2 = 1.0f;
+    public float ntheta2 = 0.0f;
+    public float nphi2 = 0.5f;
+
+    public int lengthOfLineRenderer = 1000;
+    public float widthMultiplier = .05f;
+
+    void Start()
+    {
+        LineRenderer geodesic = gameObject.AddComponent<LineRenderer>();
+        geodesic.widthMultiplier = widthMultiplier;
+        geodesic.positionCount = lengthOfLineRenderer;
+    }
+
+    void Update()
+    {
+        LineRenderer geodesic = gameObject.GetComponent<LineRenderer>();
+
+        Vector3 p = new Vector3(
+            Mathf.Sin(nphi1 * Mathf.PI) * Mathf.Cos(ntheta1 * Mathf.PI),
+            Mathf.Cos(nphi1 * Mathf.PI),
+            Mathf.Sin(nphi1 * Mathf.PI) * Mathf.Sin(ntheta1 * Mathf.PI)) * rho1;
+        Vector3 q = new Vector3(
+            Mathf.Sin(nphi2 * Mathf.PI) * Mathf.Cos(ntheta2 * Mathf.PI),
+            Mathf.Cos(nphi2 * Mathf.PI),
+            Mathf.Sin(nphi2 * Mathf.PI) * Mathf.Sin(ntheta2 * Mathf.PI)) * rho2;
+
+        float angleBetween = Mathf.Acos(Vector3.Dot(p.normalized, q.normalized));
+        var points = new Vector3[lengthOfLineRenderer];
+
+
+        if (angleBetween == 0 | angleBetween == Mathf.PI)
+        {
+            for (int i = 0; i < lengthOfLineRenderer; i++)
+            {
+                float it = i / (lengthOfLineRenderer - 1);
+                Vector3 point_i = (1 - it) * p + it * q;
+                points[i] = point_i;
+            }
+        }
+        else
+        {
+            Vector3 pqMid = (p + q) / 2; //midpoint of p and q
+            Vector3 pOrth = (pqMid - Vector3.Project(pqMid, p)).normalized; // vector normal to p in plane
+            float pMid = 0.5f * (1 + Mathf.Pow(p.magnitude, -2));//Midpoint of p and inversion of p
+            float qMid = 0.5f * (1 + Mathf.Pow(q.magnitude, -2));
+            float orthLength = Mathf.Abs(qMid - pMid * Mathf.Cos(angleBetween)) / Mathf.Sin(angleBetween);
+
+            Vector3 center = pMid * p + orthLength * pOrth;
+            float newAngle = Mathf.Acos(Vector3.Dot((center-p).normalized,(center-q).normalized)); // angle between cp and cq
+            float rad = (center - p).magnitude;
+            Vector3 cos = (center - p).normalized;
+            Vector3 sin = ((center - q) - Vector3.Project(center - q, cos)).normalized;
+
+            for (int i = 0; i < lengthOfLineRenderer; i++)
+            {
+                float angle_i = i * newAngle / (lengthOfLineRenderer - 1);
+                //float angle_i = i * 2 * Mathf.PI / (lengthOfLineRenderer - 1);
+                Vector3 point_i = center - rad * (Mathf.Cos(angle_i) * cos + Mathf.Sin(angle_i) * sin);
+                points[i] = point_i;
+            }
+        }
+
+
+        geodesic.SetPositions(points);
+    }
+}
---
+++ HyperbolicTriangle.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperbolicTriangle : MonoBehaviour
+{
+    //Set vertices in spherical coordinates
+    public float rho1 = 1.0f; // radius, <=1
+    public float ntheta1 = 0.0f; // azimuthal angle/pi
+    public float nphi1 = 0.0f; // angle of elevation/pi
+
+    public float rho2 = 1.0f;
+    public float ntheta2 = 0.0f;
+    public float nphi2 = 0.5f;
+
+    public float rho3 = 1.0f;
+    public float ntheta3 = 0.5f;
+    public float nphi3 = 0.5f;
+
+    public int lengthPerLine = 1000;
+    public float widthMultiplier = .05f;
+
+    void Start()
+    {
+        LineRenderer triangle = gameObject.AddComponent<LineRenderer>();
+        triangle.widthMultiplier = widthMultiplier;
+        triangle.positionCount = 3 * lengthPerLine;
+    }
+
+    void Update()
+    {
+        LineRenderer triangle = gameObject.GetComponent<LineRenderer>();
+
+        Vector3 v1 = new Vector3(
+            Mathf.Sin(nphi1 * Mathf.PI) * Mathf.Cos(ntheta1 * Mathf.PI),
+            Mathf.Cos(nphi1 * Mathf.PI),
+            Mathf.Sin(nphi1 * Mathf.PI) * Mathf.Sin(ntheta1 * Mathf.PI)) * rho1;
+        Vector3 v2 = new Vector3(
+            Mathf.Sin(nphi2 * Mathf.PI) * Mathf.Cos(ntheta2 * Mathf.PI),
+            Mathf.Cos(nphi2 * Mathf.PI),
+            Mathf.Sin(nphi2 * Mathf.PI) * Mathf.Sin(ntheta2 * Mathf.PI)) * rho2;
+        Vector3 v3 = new Vector3(
+            Mathf.Sin(nphi3 * Mathf.PI) * Mathf.Cos(ntheta3 * Mathf.PI),
+            Mathf.Cos(nphi3 * Mathf.PI),
+            Mathf.Sin(nphi3 * Mathf.PI) * Mathf.Sin(ntheta3 * Mathf.PI)) * rho3;
+
+        float angle1 = Mathf.Acos(Vector3.Dot(v1.normalized, v2.normalized));
+        float angle2 = Mathf.Acos(Vector3.Dot(v2.normalized, v3.normalized));
+        float angle3 = Mathf.Acos(Vector3.Dot(v3.normalized, v1.normalized));
+
+        var points = new Vector3[3 * lengthPerLine];
+
+        if (angle1 == 0 | angle1 == Mathf.PI)
+        {
+            for (int i = 0; i < lengthPerLine; i++)
+            {
+                float it = i / (lengthPerLine - 1);
+                Vector3 point_i = (1 - it) * v1 + it * v2;
+                points[i] = point_i;
+            }
+        }
+        else
+        {
+            Vector3 mid12 = (v1 + v2) / 2; //midpoint of p and q
+            Vector3 orth1 = (mid12 - Vector3.Project(mid12, v1)).normalized; // vector normal to p in plane
+            float mid1 = 0.5f * (1 + Mathf.Pow(v1.magnitude, -2));//Midpoint of p and inversion of p
+            float mid2 = 0.5f * (1 + Mathf.Pow(v2.magnitude, -2));
+            float orthLength = Mathf.Abs(mid2 - mid1 * Mathf.Cos(angle1)) / Mathf.Sin(angle1);
+
+            Vector3 center = mid1 * v1 + orthLength * orth1;
+            float newAngle = Mathf.Acos(Vector3.Dot((center - v1).normalized, (center - v2).normalized)); // angle between cp and cq
+            float rad = (center - v1).magnitude;
+            Vector3 cos = (center - v1).normalized;
+            Vector3 sin = ((center - v2) - Vector3.Project(center - v2, cos)).normalized;
+
+            for (int i = 0; i < lengthPerLine; i++)
+            {
+                //float angle_i = i * newAngle / (lengthPerLine - 1);
+                float angle_i = i * 2 * Mathf.PI / (lengthPerLine - 1);
+                Vector3 point_i = center - rad * (Mathf.Cos(angle_i) * cos + Mathf.Sin(angle_i) * sin);
+                points[i] = point_i;
+            }
+        }
+
+        if (angle2 == 0 | angle2 == Mathf.PI)
+        {
+            for (int i = 0; i < lengthPerLine; i++)
+            {
+                float it = i / (lengthPerLine - 1);
+                Vector3 point_i = (1 - it) * v2 + it * v3;
+                points[i + lengthPerLine] = point_i;
+            }
+        }
+        else
+        {
+            Vector3 mid23 = (v2 + v3) / 2; //midpoint of p and q
+            Vector3 orth2 = (mid23 - Vector3.Project(mid23, v2)).normalized; // vector normal to p in plane
+            float mid2 = 0.5f * (1 + Mathf.Pow(v2.magnitude, -2));//Midpoint of p and inversion of p
+            float mid3 = 0.5f * (1 + Mathf.Pow(v3.magnitude, -2));
+            float orthLength = Mathf.Abs(mid3 - mid2 * Mathf.Cos(angle2)) / Mathf.Sin(angle2);
+
+            Vector3 center = mid2 * v2 + orthLength * orth2;
+            float newAngle = Mathf.Acos(Vector3.Dot((center - v2).normalized, (center - v3).normalized)); // angle between cp and cq
+            float rad = (center - v2).magnitude;
+            Vector3 cos = (center - v2).normalized;
+            Vector3 sin = ((center - v3) - Vector3.Project(center - v3, cos)).normalized;
+
+            for (int i = 0; i < lengthPerLine; i++)
+            {
+                //float angle_i = i * newAngle / (lengthPerLine - 1);
+                float angle_i = i * 2 * Mathf.PI / (lengthPerLine - 1);
+                Vector3 point_i = center - rad * (Mathf.Cos(angle_i) * cos + Mathf.Sin(angle_i) * sin);
+                points[i + lengthPerLine] = point_i;
+            }
+        }
+
+        if (angle3 == 0 | angle3 == Mathf.PI)
+        {
+            for (int i = 0; i < lengthPerLine; i++)
+            {
+                float it = i / (lengthPerLine - 1);
+                Vector3 point_i = (1 - it) * v3 + it * v1;
+                points[i + 2 * lengthPerLine] = point_i;
+            }
+        }
+        else
+        {
+            Vector3 mid31 = (v3 + v1) / 2; //midpoint of p and q
+            Vector3 orth3 = (mid31 - Vector3.Project(mid31, v3)).normalized; // vector normal to p in plane
+            float mid3 = 0.5f * (1 + Mathf.Pow(v3.magnitude, -2));//Midpoint of p and inversion of p
+            float mid1 = 0.5f * (1 + Mathf.Pow(v1.magnitude, -2));
+            float orthLength = Mathf.Abs(mid1 - mid3 * Mathf.Cos(angle3)) / Mathf.Sin(angle3);
+
+            Vector3 center = mid3 * v3 + orthLength * orth3;
+            float newAngle = Mathf.Acos(Vector3.Dot((center - v3).normalized, (center - v1).normalized)); // angle between cp and cq
+            float rad = (center - v3).magnitude;
+            Vector3 cos = (center - v3).normalized;
+            Vector3 sin = ((center - v1) - Vector3.Project(center - v1, cos)).normalized;
+
+            for (int i = 0; i < lengthPerLine; i++)
+            {
+                //float angle_i = i * newAngle / (lengthPerLine - 1);
+                float angle_i = i * 2 * Mathf.PI / (lengthPerLine - 1);
+                Vector3 point_i = center - rad * (Mathf.Cos(angle_i) * cos + Mathf.Sin(angle_i) * sin);
+                points[i + 2 * lengthPerLine] = point_i;
+            }
+
+        }
+
+        triangle.SetPositions(points);
+    }
+}
